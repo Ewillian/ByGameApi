1cf8fdc6859dc90aec961b87dd83623fcb8e7480
Ewillian <guillaume.lecoq@ynov.com>
2025-07-04 16:23:58 +0200

    feat: exclude constant from coverage

--- src/Domain/Constants.cs
+++ src/Domain/Constants.cs
@@ -1,5 +1,8 @@
-﻿namespace ByGameApi.Domain;
+﻿using System.Diagnostics.CodeAnalysis;
 
+namespace ByGameApi.Domain;
+
+[ExcludeFromCodeCoverage]
 public static class Constants
 {
     public const string BadRequestTitle = "BadRequest";
@@ -10,5 +13,6 @@ public static class Constants
 
     public const string BadRequestMessage = "The request was not processed because it lacked required data.";
     public const string InternalErrorMessage = "Somethin went wrong or is not handle by the service.";
+    public const string ScoreNotFoundMessage = "The requested score(s) was not found.";
 
 }
